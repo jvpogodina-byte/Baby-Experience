9dc5ed3dd7774c9dccc8d7d6a77c1075f31e5bf0
Julia Pogodina <julia@macbookair.home>
2026-06-02 22:57:09 +0300

    Sort admin items alphabetically

--- app/admin/items/page.tsx
+++ app/admin/items/page.tsx
@@ -27,7 +27,7 @@ export default async function AdminItemsPage({ searchParams }: Props) {
         }
       }
     },
-    orderBy: [{ updatedAt: "desc" }, { title: "asc" }]
+    orderBy: { title: "asc" }
   });
 
   return (
